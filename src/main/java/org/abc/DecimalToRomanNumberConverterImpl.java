161510552af9358eeb03d90b4d1c5a629b6311f5
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2024-07-19 22:17:39 +0400

    feat: implement decimal to roman number converter

---
+++ src/main/java/org/abc/DecimalToRomanNumberConverterImpl.java
@@ -0,0 +1,37 @@
+package org.abc;
+
+/**
+ * This class implements the NumberConverter interface to convert decimal numbers to Roman numerals.
+ */
+public class DecimalToRomanNumberConverterImpl implements NumberConverter<Integer, String> {
+
+    private static final int[] DECIMALS = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] ROMAN_NUMERALS = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    /**
+     * Converts a decimal number to its Roman numeral representation.
+     *
+     * @param source the decimal number to be converted
+     * @return the Roman numeral representation of the given decimal number
+     */
+    @Override
+    public String convert(Integer source) {
+        if (source == null || source <= 0) {
+            throw new IllegalArgumentException("Input must be a positive integer.");
+        }
+
+        int i = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+        while (source > 0) {
+            if (source >= DECIMALS[i]) {
+                stringBuilder.append(ROMAN_NUMERALS[i]);
+                source -= DECIMALS[i];
+            } else {
+                i++;
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+}
+
--- src/main/java/org/abc/Main.java
+++ src/main/java/org/abc/Main.java
@@ -3,5 +3,9 @@ package org.abc;
 public class Main {
 
     public static void main(String[] args) {
+
+        NumberConverter converter = new DecimalToRomanNumberConverterImpl();
+
+        System.out.println(converter.convert(1236));
     }
 }
