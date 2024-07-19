a38b4ae406bb016f2757d8c58fe3f2c9f0b97527
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2024-07-20 02:11:18 +0400

    chore: refactor main class

---
+++ src/main/java/org/abc/NumberConverterApplication.java
@@ -0,0 +1,13 @@
+package org.abc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumberConverterApplication.class, args);
+    }
+
+}
