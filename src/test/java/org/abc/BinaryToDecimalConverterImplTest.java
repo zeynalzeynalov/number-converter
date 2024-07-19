9b043c193d1b3afe1c353a83156ed56c3d623d1f
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2024-07-20 00:26:44 +0400

    chore: restructure tests for binary to decimal converter

--- src/test/java/org/abc/BinaryToDecimalConverterImplTest.java
+++ src/test/java/org/abc/BinaryToDecimalConverterImplTest.java
@@ -9,9 +9,16 @@ class BinaryToDecimalConverterImplTest {
     private final BinaryToDecimalConverterImpl binaryToDecimalConverter = new BinaryToDecimalConverterImpl();
 
     @Test
-    public void convert_AllRomanNumerals_Correct() {
-        Integer actual = binaryToDecimalConverter.convert("10");
+    public void convert_Zero_Correct() {
+        Integer actual = binaryToDecimalConverter.convert("0");
 
-        assertEquals(2, actual);
+        assertEquals(0, actual);
+    }
+
+    @Test
+    public void convert_One_Correct() {
+        Integer actual = binaryToDecimalConverter.convert("1");
+
+        assertEquals(1, actual);
     }
 }
