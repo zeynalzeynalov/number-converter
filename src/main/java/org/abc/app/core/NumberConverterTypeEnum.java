13adb02bfb5b03451f37b4a5715033dc75d57a4a
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2024-07-20 22:50:01 +0400

    feat: introduce converter manager for choosing right converter

---
+++ src/main/java/org/abc/app/core/NumberConverterTypeEnum.java
@@ -0,0 +1,21 @@
+package org.abc.app.core;
+
+/**
+ * Enum representing different types of number conversions.
+ */
+public enum NumberConverterTypeEnum {
+    /**
+     * Conversion from decimal to Roman numerals.
+     */
+    DECIMAL_TO_ROMAN,
+
+    /**
+     * Conversion from binary to Roman numerals.
+     */
+    BINARY_TO_ROMAN,
+
+    /**
+     * Conversion from binary to decimal.
+     */
+    BINARY_TO_DECIMAL
+}
--- src/main/java/org/abc/app/numberconverter/NumberConverterService.java
+++ src/main/java/org/abc/app/numberconverter/NumberConverterService.java
@@ -1,20 +1,42 @@
 package org.abc.app.numberconverter;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.abc.app.core.NumberConverter;
+import org.abc.app.core.NumberConverterTypeEnum;
+import org.springframework.stereotype.Service;
 
-@Component
+import java.util.Optional;
+
+/**
+ * Service class for handling number conversion logic.
+ */
+@Service
 @RequiredArgsConstructor
 public class NumberConverterService {
 
-    DecimalToRomanConverterImpl decimalToRomanConverter = new DecimalToRomanConverterImpl();
-    BinaryToDecimalConverterImpl binaryToDecimalConverter = new BinaryToDecimalConverterImpl();
+    private final NumberConverterManager numberConverterManager;
 
-    public String convertDecimalToRoman(Integer input) {
-        return decimalToRomanConverter.convert(input);
-    }
+    /**
+     * Converts a number based on the provided request.
+     *
+     * @param request the request DTO containing the input and conversion type
+     * @return the result of the conversion
+     * @throws IllegalArgumentException if the converter type is unknown or the input is invalid
+     */
+    public String convert(RequestConvertDTO request) {
+        try {
+            NumberConverterTypeEnum numberConverterTypeEnum = NumberConverterTypeEnum.valueOf(request.getType().toUpperCase());
+            Optional<NumberConverter> numberConverter = numberConverterManager.getConverter(numberConverterTypeEnum);
 
-    public Integer convertBinaryToDecimal(String input) {
-        return binaryToDecimalConverter.convert(input);
+            if (numberConverter.isPresent()) {
+                return numberConverter.get().convert(request.getInput());
+            } else {
+                throw new IllegalArgumentException("Converter not found for type: " + request.getType());
+            }
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid converter type: " + request.getType(), e);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error during conversion", e);
+        }
     }
 }
