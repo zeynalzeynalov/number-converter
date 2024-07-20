162d0119d480a9d7236a3ef2eaf757112c12d59e
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2024-07-20 22:47:21 +0400

    feat: change converter interface to get and return string

---
+++ src/main/java/org/abc/app/core/NumberConverter.java
@@ -0,0 +1,25 @@
+package org.abc.app.core;
+
+/**
+ * Interface for converting numbers between different formats.
+ */
+public interface NumberConverter {
+
+    /**
+     * Converts the input number to a different format.
+     *
+     * @param input the number to be converted, as a string
+     * @return the converted number, as a string
+     * @throws IllegalArgumentException if the input is invalid or cannot be converted
+     */
+    String convert(String input);
+
+    /**
+     * Validates the input number.
+     *
+     * @param input the number to be validated, as a string
+     * @return true if the input is valid, false otherwise
+     * @throws IllegalArgumentException if the input is null, empty, or not valid according to specific conversion rules
+     */
+    boolean isValid(String input) throws IllegalArgumentException;
+}
