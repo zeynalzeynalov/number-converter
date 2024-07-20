5042268358449b5e52fafd066488853f0bd09e3d
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2024-07-20 22:51:14 +0400

    feat: update controller according to service changes

--- src/main/java/org/abc/app/numberconverter/NumberConverterController.java
+++ src/main/java/org/abc/app/numberconverter/NumberConverterController.java
@@ -7,39 +7,40 @@ import org.springframework.web.bind.annotation.*;
 
 import static org.abc.app.core.RestResponse.FAIL;
 
-@RequestMapping("/api/numberconverter")
+/**
+ * REST controller for handling number conversion requests.
+ */
+@RequestMapping("/api/number-converter")
 @RestController
 @RequiredArgsConstructor
 public class NumberConverterController {
 
+    // The service that handles the number conversion logic
     private final NumberConverterService numberConverterService;
 
+    /**
+     * Endpoint to check the status of the API.
+     *
+     * @return ResponseEntity containing a status message
+     */
     @GetMapping("/status")
-    ResponseEntity<RestResponse> checkStatus() {
-
+    public ResponseEntity<RestResponse> checkStatus() {
         return ResponseEntity.ok(new RestResponse("API is running."));
     }
 
-    @PostMapping("decimaltoroman")
-    public ResponseEntity<RestResponse> convertDecimalToRoman(
-            @RequestBody RequestDecimalToRomanDTO request) {
+    /**
+     * Endpoint to convert numbers based on the provided request.
+     *
+     * @param request the request DTO containing the conversion parameters
+     * @return ResponseEntity containing the conversion result or an error message
+     */
+    @PostMapping("/convert")
+    public ResponseEntity<RestResponse> convert(@RequestBody RequestConvertDTO request) {
         try {
-            String result = numberConverterService.convertDecimalToRoman(request.input);
+            String result = numberConverterService.convert(request);
             return ResponseEntity.ok(new RestResponse(result));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(new RestResponse(FAIL, e.getMessage()));
         }
     }
-
-    @PostMapping("binarytodecimal")
-    public ResponseEntity<RestResponse> convertBinaryToDecimal(
-            @RequestBody RequestBinaryToDecimalDTO request) {
-        try {
-            Integer result = numberConverterService.convertBinaryToDecimal(request.input);
-            return ResponseEntity.ok(new RestResponse(result));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(new RestResponse(FAIL, e.getMessage()));
-        }
-    }
-
 }
